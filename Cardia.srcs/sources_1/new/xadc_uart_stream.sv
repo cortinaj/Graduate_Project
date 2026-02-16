7e41879ce81a20a2e0c22354a24261c06a79c02c
cortinaj <johnpaul.cortina.716@my.csun.edu>
2026-02-15 19:11:25 -0800

    Instantiated custom UART IP

--- Cardia.srcs/sources_1/new/xadc_uart_stream.sv
+++ Cardia.srcs/sources_1/new/xadc_uart_stream.sv
@@ -23,25 +23,32 @@
 module xadc_uart_stream(
     input logic clk,
     input logic [7:0] ja,
-    input logic TX,
-    output logic RX,
+    input logic RX,
+    output logic TX,
     output logic [3:0] led
     );
     
+    //Wires fo XADC
     logic [6:0] daddr; //ddress of channel to be read
     logic eoc; //xadc end of conversion flag
     logic [15:0] dout; //adc data out bus
     logic drdy;
-    
     logic [1:0] _drdy = 0; // delayed data ready signal for edge detection
     logic [7:0] data = 0; // stored xadc data, only the uppoermost byte
     
-    xadc_wix_0 myxadc(
+    //Wires for UART
+    logic [7:0] rx_data; //Received byte
+    logic rx_ready; //Pulse when byte received
+    logic [7:0] tx_data = 8'd0;
+    logic tx_start = 1'b0;
+    
+    //Instantiate IP's
+    xadc_wiz_0 myxadc(
         .dclk_in (clk),
         .den_in (eoc), //drp enable, start a new conversion whenever the last one has ended
-        .dwe_in (0),
+        .dwe_in (1'b0),
         .daddr_in (daddr), //channel address
-        .di_in (0),
+        .di_in (16'd0),
         .do_out (dout), // data out
         .drdy_out (drdy), // data ready
         .eoc_out (eoc), // end of conversion
@@ -49,10 +56,21 @@ module xadc_uart_stream(
         .vauxn14        (ja[4]),
         .vauxp14        (ja[0]),
         
-        .vn_in          (0),
-        .vp_in          (0)
+        .vn_in          (1'b0),
+        .vp_in          (1'b0)
         );
         
+   UART_IP_0 myuart(
+        .sys_clk (clk),
+        .TX (TX),
+        .RX(RX),
+        .TxD_par (rx_data),
+        .TxD_ready (rx_ready),
+        .RxD_par (tx_data),
+        .Rxd_start (tx_start)
+        );
+                      
+  //Logic      
     always_ff@(posedge clk) begin
         _drdy <= {_drdy[0], drdy};
     end
