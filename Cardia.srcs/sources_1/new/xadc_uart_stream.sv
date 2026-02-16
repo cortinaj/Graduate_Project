37025dad6c5e30c41c26e304e8454c25d3fb8705
cortinaj <johnpaul.cortina.716@my.csun.edu>
2026-02-15 18:52:00 -0800

    Updated the axi_uart_stream module by incorporating tutorial logic and modifying it.

--- Cardia.srcs/sources_1/new/xadc_uart_stream.sv
+++ Cardia.srcs/sources_1/new/xadc_uart_stream.sv
@@ -27,4 +27,42 @@ module xadc_uart_stream(
     output logic RX,
     output logic [3:0] led
     );
+    
+    logic [6:0] daddr; //ddress of channel to be read
+    logic eoc; //xadc end of conversion flag
+    logic [15:0] dout; //adc data out bus
+    logic drdy;
+    
+    logic [1:0] _drdy = 0; // delayed data ready signal for edge detection
+    logic [7:0] data = 0; // stored xadc data, only the uppoermost byte
+    
+    xadc_wix_0 myxadc(
+        .dclk_in (clk),
+        .den_in (eoc), //drp enable, start a new conversion whenever the last one has ended
+        .dwe_in (0),
+        .daddr_in (daddr), //channel address
+        .di_in (0),
+        .do_out (dout), // data out
+        .drdy_out (drdy), // data ready
+        .eoc_out (eoc), // end of conversion
+        
+        .vauxn14        (ja[4]),
+        .vauxp14        (ja[0]),
+        
+        .vn_in          (0),
+        .vp_in          (0)
+        );
+        
+    always_ff@(posedge clk) begin
+        _drdy <= {_drdy[0], drdy};
+    end
+    
+    assign daddr = 7'h1E;
+    
+    always_ff@(posedge clk) begin
+        if(_drdy == 2'b10) begin // on negative edge
+            data <= dout[15:8];
+            end
+    end
+    
 endmodule
